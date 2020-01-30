5325bdd508a62261db71d3c56a5ad5e128ea0d87
Nadia H. Pedersen <nadia@nhp.sh>
2020-01-30 18:00:36 +0100

    Adapt memory setup code from the x64 JIT to the aarch64 JIT

--- src/ARMJIT_A64/ARMJIT_Compiler.cpp
+++ src/ARMJIT_A64/ARMJIT_Compiler.cpp
@@ -6,6 +6,9 @@
 
 #ifdef __SWITCH__
 #include "../switch/compat_switch.h"
+#else
+#include <sys/mman.h>
+#include <unistd.h>
 #endif
 
 #include <malloc.h>
@@ -33,6 +36,9 @@ template <>
 const int RegisterCache<Compiler, ARM64Reg>::NativeRegsAvailable = 8;
 
 const int JitMemSize = 16 * 1024 * 1024;
+#ifndef __SWITCH__
+u8 JitMem[JitMemSize];
+#endif
 
 void Compiler::MovePC()
 {
@@ -74,6 +80,15 @@ Compiler::Compiler()
 
     SetCodeBase((u8*)JitRWStart, (u8*)JitRXStart);
     JitMemUseableSize = JitMemSize;
+    Reset();
+#else
+    u64 pageSize = sysconf(_SC_PAGE_SIZE);
+    u8* pageAligned = (u8*)(((u64)JitMem & ~(pageSize - 1)) + pageSize);
+    u64 alignedSize = (((u64)JitMem + sizeof(JitMem)) & ~(pageSize - 1)) - (u64)pageAligned;
+    mprotect(pageAligned, alignedSize, PROT_EXEC | PROT_READ | PROT_WRITE);
+    
+    SetCodeBase(pageAligned, pageAligned);
+    JitMemUseableSize = alignedSize;
     Reset();
 #endif
 
